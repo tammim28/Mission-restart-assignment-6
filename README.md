0bec482efae63990b582a1f4b0c47d4766a2e011
tammim28 <tammimhasan123@gmail.com>
2026-03-03 22:05:43 +0600

    readme file added

---
+++ README.md
@@ -0,0 +1,32 @@
+# undefined
+ যখন কোনো ভেরিয়েবল declare করা হয় কিন্তু কোনো value দেওয়া হয় না, তখন সেটি undefined থাকে।
+
+# null
+ ইচ্ছা করে কোনো ভেরিয়েবলকে খালি বা empty দেখাতে null ব্যবহার করা হয়।
+
+# map()
+Array এর প্রতিটি element নিয়ে কাজ করে নতুন একটি array return করে
+
+# forEach()
+শুধু array loop করে, কোনো নতুন array return করে না
+
+# == (Loose Equality)
+শুধু value compare করে, type check করে না
+
+# === (Strict Equality)
+value এবং type দুটোই compare করে
+
+# API fetch করার ক্ষেত্রে async/await এর গুরুত্ব
+async/await ব্যবহার করলে asynchronous code সহজ ও readable হয়। try/catch দিয়ে error handle করা সহজ হয়
+
+# JavaScript এ Scope কী?
+Scope মানে হলো কোনো ভেরিয়েবল কোথায় ব্যবহার করা যাবে।
+
+## Global Scope
+পুরো প্রোগ্রাম জুড়ে ব্যবহার করা যায়।
+
+## Function Scope
+শুধু function এর ভিতরে ব্যবহার করা যায়।
+
+## Block Scope
+শুধু {} ব্লকের ভিতরে ব্যবহার করা যায় (let এবং const এর ক্ষেত্রে)।
--- script/script.js
+++ script/script.js
@@ -1,20 +1,74 @@
-const loadCategory = () =>{
-    fetch("https://fakestoreapi.com/products/categories")
-    .then((res)=> res.json())
+const loadCategory = () => {
+  fetch("https://fakestoreapi.com/products/categories")
+    .then((res) => res.json())
     .then((json) => displayCategory(json));
 };
 
-const displayCategory = (categories) =>{
-    const categoryContainer = document.getElementById("category-container");
-    categoryContainer.innerHTML = "";
-    for(let category of categories){
-        console.log(category)
-        const btnDiv = document.createElement("div");
-        btnDiv.innerHTML = `
-        <button class="btn btn-soft btn-primary">${category}</button>
-        `
-        categoryContainer.append(btnDiv);
-    }
-}
+
+const loadCategoryProduct = (category) => {
+  fetch("https://fakestoreapi.com/products")
+    .then((res) => res.json())
+    .then((data) => {
+      const filteredProducts = data.filter(
+        (product) => product.category === category
+      );
+
+      displayProducts(filteredProducts);
+    });
+};
+
+
+
+const displayProducts = (products) => {
+  const productContainer = document.getElementById("product-container");
+  productContainer.innerHTML = "";
+
+  products.forEach((product) => {
+    const div = document.createElement("div");
+    div.className = "card bg-base-100 shadow-sm p-4";
+
+    div.innerHTML = `
+      <img src="${product.image}" class="h-40 mx-auto bg-gray-300 p-4 rounded-xl object-contain"/>
+      <div class = "flex items-center justify-between mt-2">
+        <p class = "bg-slate-200 py-1 px-2 rounded-2xl text-blue-900">${product.category}</p>
+        <div class = "flex items-center gap-1">
+            <i class="fa-regular text-yellow-900 fa-star"></i>
+            <p>${product.rating.rate}</p>
+            <p>(${product.rating.count})</p>
+        </div>
+    </div>
+      <h2 class="font-semibold text-base mt-2 truncate">${product.title}</h2>
+      <p class="text-lg font-bold">$${product.price}</p>
+      <div class = "flex justify-between pt-2">
+        <button  onclick="document.getElementById('my_modal_5').showModal()" class="btn btn-soft btn-primary">
+        <i class="fa-regular fa-eye"></i> Details</button>
+        <button  class="btn btn-soft btn-primary">
+        <i class="fa-solid fa-cart-shopping"></i> Add</button>
+    </div>
+    `;
+
+    
+
+    productContainer.appendChild(div);
+  });
+};
+
+const displayCategory = (categories) => {
+  const categoryContainer = document.getElementById("category-container");
+
+  categoryContainer.innerHTML = "";
+
+  categories.forEach((category) => {
+    const btn = document.createElement("button");
+    btn.className = "btn btn-soft btn-primary";
+    btn.innerText = category;
+
+    btn.addEventListener("click", () => {
+      loadCategoryProduct(category);
+    });
+
+    categoryContainer.appendChild(btn);
+  });
+};
 
 loadCategory();
